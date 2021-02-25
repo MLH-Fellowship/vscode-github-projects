255bbec96d45b735c66b840b476c8170737e2fe0
gladishd <gladish.dean@gmail.com>
2021-02-24 21:48:48 -0600

    we can filter based on the sidebar, selecting multiple values.

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,15 +1,26 @@
 <script>
+  let filterInclude = ['Repository'];
+
+  let menu = ['Repository', 'Organization', 'Open Project', 'Closed Project'];
+
+  function join(filterInclude) {
+    if (filterInclude.length === 1) return filterInclude[0];
+    return `${filterInclude.slice(0, -1).join(', ')} and ${
+      filterInclude[filterInclude.length - 1]
+    }`;
+  }
+
   $: session = null;
 
-  window.addEventListener("message", async (event) => {
+  window.addEventListener('message', async (event) => {
     const message = event.data;
     switch (message.command) {
-      case "authComplete":
+      case 'authComplete':
         session = message.payload.session;
     }
   });
   // send message as soon as sidebar loads.
-  ext_vscode.postMessage({ type: "onSignIn", value: "success" });
+  ext_vscode.postMessage({ type: 'onSignIn', value: 'success' });
 </script>
 
 {#if !session}
@@ -18,19 +29,37 @@
     <button
       on:click={() => {
         //send message to SidebarProvider.ts
-        ext_vscode.postMessage({ type: "onSignIn", value: "success" });
+        ext_vscode.postMessage({ type: 'onSignIn', value: 'success' });
       }}
     >
       Sign in with GitHub
     </button>
   </div>
 {:else}
-  <button
-    on:click={() => {
-      //send message to SidebarProvider.ts
-      ext_vscode.postMessage({ type: "onSignIn", value: "noNotification" });
-    }}
-  >
-    See Projects
-  </button>
+  <div>
+    <button
+      on:click={() => {
+        //send message to SidebarProvider.ts
+        ext_vscode.postMessage({ type: 'onSignIn', value: 'noNotification' });
+      }}
+      class="seeProjectsButton"
+    >
+      See Projects
+    </button>
+
+    {#each menu as flavour}
+      <label class="labels">
+        <input
+          type="checkbox"
+          bind:group={filterInclude}
+          value={flavour}
+          on:click={() => {
+            console.log(join(filterInclude));
+          }}
+          class="checkBox"
+        />
+        {flavour}
+      </label>
+    {/each}
+  </div>
 {/if}
