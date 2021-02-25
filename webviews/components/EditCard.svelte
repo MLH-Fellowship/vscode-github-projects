fc7ae65e50eab74abe6eeb34b519983c99891471
Shrill Shrestha <shrill.shrestha@gmail.com>
2021-02-25 09:41:08 -0600

    include edit field to edit existing card

---
+++ webviews/components/EditCard.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { getContext } from 'svelte';
+  import EditField from './EditField.svelte';
+
+  const { open } = getContext('simple-modal');
+
+  const edit_card = () => {
+    open(EditField, { message: "" }, {closeButton: ""});
+  };
+
+  const deleteCard = () => {
+    //add mutation for deleting card
+  }
+</script>
+
+<div>
+  
+  <div style=" display:flex; flex-direction: row; margin:5px;">
+    <button on:click={edit_card}>Edit Card</button>
+    <button on:click={deleteCard}>
+      Delete
+    </button>
+  </div>
+</div>
---
+++ webviews/components/EditField.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { getContext } from 'svelte';
+
+  const { close } = getContext('simple-modal');
+  let text = ""
+
+  const closeAdd = () => {
+    console.log(text);
+    //add mutations for editing card
+    close();
+  };
+</script>
+
+<div>
+  <input bind:value={text} />
+  <button on:click={closeAdd}>
+    Edit
+  </button>
+</div>
