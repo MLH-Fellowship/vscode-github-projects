6a15c93980a35b4dbbd6dc9aacf1732b7ff409f7
Shrill Shrestha <shrill.shrestha@gmail.com>
2021-02-25 09:40:39 -0600

    include edit field to add card

---
+++ webviews/components/AddCard.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { getContext } from 'svelte';
+
+  const { close } = getContext('simple-modal');
+  let text = ""
+
+  const closeAdd = () => {
+    console.log(text);
+    //add mutations for adding card
+    close();
+  };
+</script>
+
+<div>
+  <form on:submit|preventDefault>
+    <input bind:value={text} />
+    <button on:click={closeAdd} type="submit">
+      Add
+    </button>
+  </form>
+</div>
---
+++ webviews/components/Content.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { getContext } from 'svelte';
+  import AddCard from './AddCard.svelte';
+
+  const { open } = getContext('simple-modal');
+
+  const showSurprise = () => {
+    open(AddCard, { message: "Add Card" }, {closeButton: ""});
+  };
+</script>
+
+<p><button on:click={showSurprise}>Add Card</button></p>
