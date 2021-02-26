8b779b8dbe5ffa8aa0b37162ae4c02631e0ba9fe
Shrill Shrestha <shrill.shrestha@gmail.com>
2021-02-25 23:48:38 -0600

    Add convert to issue ui

---
+++ webviews/components/ConvertIssue.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { getContext } from 'svelte';
+  export let message;
+  const { close } = getContext('simple-modal');
+  let body = ""
+
+  const convertIssue = () => {
+    //body is issue body and message is title
+    //add mutations for converting to issue
+    close();
+  };
+</script>
+
+<div>
+  <input bind:value={body} placeholder="Add a body for issue" />
+  <div style="display: flex; flex-direction: row; width: 100%;">
+    <button on:click={convertIssue} style="width:50%; margin-right: 5px;">
+      Confirm
+    </button>
+    <button on:click={convertIssue} style="width:50%">
+      Cancel
+    </button>
+  </div>
+</div>
--- webviews/components/EditCard.svelte
+++ webviews/components/EditCard.svelte
@@ -1,6 +1,7 @@
 <script>
   import { getContext, createEventDispatcher } from "svelte";
   import EditField from "./EditField.svelte";
+  import ConvertIssue from "./ConvertIssue.svelte";
   export let note;
 
   const { open } = getContext("simple-modal");
@@ -27,15 +28,41 @@
     );
   };
 
+  const convertToIssue = () => {
+    //note is the title for the card.
+    open(ConvertIssue,
+    { message: note },
+    { closeButton: "" },
+    {
+        onOpen: () => {
+          dispatch("message", {
+            payload: "stopDrag",
+          });
+        },
+        onClose: () => {
+          dispatch("message", {
+            payload: "startDrag",
+          });
+        },
+      }
+    );
+  }
+
   const deleteCard = () => {
     //add mutation for deleting card
   };
+
+  const archiveCard = () => {
+    //add mutation to archive card
+  }
+
 </script>
 
 <div>
   <div style="display:flex; flex-direction: row; margin-top:5px;">
     <button on:click={edit_card} style="margin-right:5px;">Edit</button>
     <button on:click={deleteCard} style="margin-right:5px;"> Delete </button>
-    <button on:click={deleteCard}> Archive </button>
+    <button on:click={archiveCard} style="margin-right:5px;"> Archive </button>
+    <button on:click={convertToIssue}>Convert to Issue</button>
   </div>
 </div>
